287e4dbed5afc6a7a1bdf651770b84ede57e3c39
Pawan Gola <pawangola908@gmail.com>
2024-02-06 18:46:49 +0530

    object

---
+++ objects/ObjThree.js
@@ -0,0 +1,26 @@
+
+const course = {
+    coursename: "js ",
+    pice: "999",
+    courseInstructor: "keshav"
+}
+
+//course.courseInstructor;
+
+//..for better code understanding use this...
+const {courseInstructor: instuctor} = course;
+
+console.log(instuctor);
+
+//..react part..
+// const navbar = ({company}) =>{
+       
+// }
+// navbar(company = "keshav")
+
+//..Api...
+[
+   {},
+   {},
+   {}
+]
--- objects/ObjeTwo.js
+++ objects/ObjeTwo.js
@@ -30,4 +30,26 @@ const obj2 = {3: "a", 4: "b"}
 const obj3 = Object.assign(obj1, obj2)
 console.log(obj3);
 
+// pass database value 
 
+const user = [
+    {
+       id:1,
+       email: "h@gmail.com"
+    },
+    {
+
+    },
+    {
+
+    },
+]
+
+user[1].email
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+ 
